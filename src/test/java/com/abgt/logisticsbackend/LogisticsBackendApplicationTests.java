abdd22702e9c7053134ee9a4038c4d6f7f419d96
Abhijeet Saxena <absaxena@griddynamics.com>
2026-03-16 14:36:14 +0530

    Initial backend setup

---
+++ src/test/java/com/abgt/logisticsbackend/LogisticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abgt.logisticsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
